7f25d73af7e192ca2a62765edd1676c56a906579
Taetae1123 <xodus2079@naver.com>
2025-02-06 18:31:26 +0900

    Fix: path로 이용

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+// app.set("views", "./views");
+app.set("views", path.join(__dirname, "/"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+app.get("/test2", (req, res) => {
+  res.render("test2");
 });
 
 app.listen(port, () => {
