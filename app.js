86955c0d854ec7e611d09b667104e3e7828a6f37
Taetae1123 <xodus2079@naver.com>
2025-02-10 15:31:36 +0900

    Feat: get요청

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const path = require("path");
+// const path = require("path");
+
+// body-parser
+// x-www-form-urlencoded 방식, 객체 형태로 결과가 나옴(객체형태: {})
+app.use(express.urlencoded({ extended: true }));
+// json
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 // app.set("views", path.join(__dirname, "/"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World!");
+  res.render("main");
+});
+
+app.get("/getForm", (req, res) => {
+  console.log(req.query, "요청 왔니?");
+  res.render("result", { title: "GET 요청 결과", userinfo: req.query });
 });
 
 app.get("/test", (req, res) => {
