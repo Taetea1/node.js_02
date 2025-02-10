3df784442efab36b2dd7198bfd3954bab17c69f7
Taetae1123 <xodus2079@naver.com>
2025-02-10 23:39:35 +0900

    Fix: 스크립트 분리

--- public/JS/test.js
+++ public/JS/test.js
@@ -1,32 +1,46 @@
-// const { response } = require("express");
-// fetch("/userinfo").then((response) => {});
-// const info = document.querySelector(".info");
-// info.innerHTML = `
-//     <table>
-//     <thead>
-//       <tr>
-//         <td>이메일</td>
-//         <td>이름</td>
-//         <td>나이</td>
-//       </tr>
-//     </thead>
-//     <tbody class="tablebody">
-//     </tbody>
-//     </table>`;
-// let datas = [];
-// // 그전값들
-// let userList = JSON.parse(localStorage.getItem("userinfo")) || [];
-// datas.push(...userList);
+// 백엔드에 요청을 다시 보냄
+fetch("/userinfo")
+  .then((response) => response.json())
+  .then((data) => {
+    // 가져온 데이터를 화면에 표시
+    const info = document.querySelector(".info");
 
-// // 새로운값
-// datas.push(userinfo);
-// localStorage.setItem("userinfo", JSON.stringify(datas));
+    info.innerHTML = `
+      <table>
+      <thead>
+        <tr>
+          <td>이메일</td>
+          <td>이름</td>
+          <td>나이</td>
+        </tr>
+      </thead>
+      <tbody class="tablebody">
+      </tbody>
+      </table>`;
 
-// const tbody = document.querySelector(".tablebody");
-// datas.map((x) => {
-//   tbody.innerHTML += `<tr>
-//         <td>${x.email}</td>
-//         <td>${x.name}</td>
-//         <td>${x.age}</td>
-//       </tr>`;
-// });
+    // 뭔가를 추가하는 내용
+    let base_data = [];
+    base_data = JSON.parse(localStorage.getItem("userinfo")) || [];
+
+    // 기존 데이터에 동일한 항목이 있는지 확인
+    const isDuplicate = base_data.some(
+      (item) => JSON.stringify(item) === JSON.stringify(data)
+    );
+    if (!isDuplicate) {
+      const update_data = JSON.parse(JSON.stringify(data));
+      base_data.push(update_data);
+      localStorage.setItem("userinfo", JSON.stringify(base_data));
+      console.log(JSON.parse(JSON.stringify(data)), "dddddddd");
+    }
+    const tablebody = document.querySelector(".tablebody");
+    base_data.map((x) => {
+      tablebody.innerHTML += `<tr>
+      <td>${x.email}</td>  
+      <td>${x.name}</td>  
+      <td>${x.age}</td>  
+    </tr>`;
+    });
+  })
+  .catch((e) => {
+    console.log("에러");
+  });
